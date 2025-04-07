82d5dc0c0c2e2dd0d674afd5bd8db0041e17d47b
pro274 <greatofalltim@gmail.com>
2025-04-07 22:18:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Add this at the **top** of your index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is online!');
+});
+
+app.listen(port, () => {
+  console.log(`Web server running on port ${port}`);
+});
 const express = require('express');
 const app = express();
 
