80a8ad49911eaec5d406d595d44a53abfc51489b
pro274 <greatofalltim@gmail.com>
2025-04-07 22:36:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Web server running on port ${port}`);
-});
-const express = require('express');
-const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Bot is running on Render!');
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Web server is running on port ${PORT}`);
-});
