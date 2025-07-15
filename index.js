49e60cc738f56665e09a1ec9cdda7fb9a74f47ab
pro274 <greatofalltim@gmail.com>
2025-07-15 20:24:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,5 +30,9 @@ function createBot() {
     console.log('⚠️ Bot error:', err.message);
   });
 }
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is alive!'));
+app.listen(3000, () => console.log('Web server running!'));
 
 createBot();
